81d4ea3fe67e3c1222ba6c5ef8b25dccf3d1222b
Mark Gandolfo <mark@aussiev8.com.au>
2014-08-30 17:49:23 +1000

    green (except for a few yellows) controller specs for posts controller

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -20,11 +20,18 @@ require 'rails_helper'
 
 RSpec.describe PostsController, :type => :controller do
 
+  before(:each) do
+    @section = FactoryGirl.create(:section)
+    @post = @section.posts.create(FactoryGirl.attributes_for(:post))
+
+    allow(Section).to receive_messages(find: @section)
+  end
+
   # This should return the minimal set of attributes required to create a valid
   # Post. As you add validations to Post, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    attributes_for(:post)
   }
 
   let(:invalid_attributes) {
@@ -34,40 +41,45 @@ RSpec.describe PostsController, :type => :controller do
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
   # PostsController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
+  let(:valid_session) { }
 
   describe "GET index" do
+    login_user
+
     it "assigns all posts as @posts" do
-      post = Post.create! valid_attributes
-      get :index, {}, valid_session
-      expect(assigns(:posts)).to eq([post])
+      get :index, { section_id: @post.section.to_param }, valid_session
+      expect(assigns(:posts)).to eq([@post])
     end
   end
 
   describe "GET show" do
     it "assigns the requested post as @post" do
-      post = Post.create! valid_attributes
-      get :show, {:id => post.to_param}, valid_session
-      expect(assigns(:post)).to eq(post)
+      get :show, {:id => @post.to_param, section_id: @post.section.to_param}, valid_session
+      expect(assigns(:post)).to eq(@post)
     end
   end
 
   describe "GET new" do
+    login_user
+
     it "assigns a new post as @post" do
-      get :new, {}, valid_session
+      get :new, {section_id: @section.to_param}, valid_session
       expect(assigns(:post)).to be_a_new(Post)
     end
   end
 
   describe "GET edit" do
+    login_user
+
     it "assigns the requested post as @post" do
-      post = Post.create! valid_attributes
-      get :edit, {:id => post.to_param}, valid_session
-      expect(assigns(:post)).to eq(post)
+      get :edit, {id: @post.to_param, section_id: @post.section.to_param}, valid_session
+      expect(assigns(:post)).to eq(@post)
     end
   end
 
   describe "POST create" do
+    login_user
+
     describe "with valid params" do
       it "creates a new Post" do
         expect {
@@ -82,8 +94,8 @@ RSpec.describe PostsController, :type => :controller do
       end
 
       it "redirects to the created post" do
-        post :create, {:post => valid_attributes}, valid_session
-        expect(response).to redirect_to(Post.last)
+        post :create, {post: valid_attributes, section_id: @section.to_param}, valid_session
+        expect(response).to redirect_to(section_post_path(@section.id, Post.last))
       end
     end
 
@@ -101,57 +113,57 @@ RSpec.describe PostsController, :type => :controller do
   end
 
   describe "PUT update" do
+    login_user
+
     describe "with valid params" do
       let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
+        { title: Faker::Lorem.words(rand(4..10), false).join(' '),
+          body: Faker::Lorem.words(rand(5..60), false).join(' ') }
       }
 
       it "updates the requested post" do
-        post = Post.create! valid_attributes
-        put :update, {:id => post.to_param, :post => new_attributes}, valid_session
-        post.reload
-        skip("Add assertions for updated state")
+        put :update, {id: @post.to_param, :post => new_attributes}, valid_session
+        @post.reload
+        new_attributes.each do |k, v|
+          expect(@post[k]).to eq(v)
+        end
       end
 
       it "assigns the requested post as @post" do
-        post = Post.create! valid_attributes
-        put :update, {:id => post.to_param, :post => valid_attributes}, valid_session
-        expect(assigns(:post)).to eq(post)
+        put :update, {id: @post.to_param, section_id: @post.section.to_param, post: valid_attributes}, valid_session
+        expect(assigns(:post)).to eq(@post)
       end
 
       it "redirects to the post" do
-        post = Post.create! valid_attributes
-        put :update, {:id => post.to_param, :post => valid_attributes}, valid_session
-        expect(response).to redirect_to(post)
+        put :update, {id: @post.to_param, section_id: @post.section.to_param, post: valid_attributes}, valid_session
+        expect(response).to redirect_to(@post)
       end
     end
 
     describe "with invalid params" do
       it "assigns the post as @post" do
-        post = Post.create! valid_attributes
-        put :update, {:id => post.to_param, :post => invalid_attributes}, valid_session
-        expect(assigns(:post)).to eq(post)
+        put :update, {id: @post.to_param, section_id: @post.section.to_param, post: invalid_attributes}, valid_session
+        expect(assigns(:post)).to eq(@post)
       end
 
       it "re-renders the 'edit' template" do
-        post = Post.create! valid_attributes
-        put :update, {:id => post.to_param, :post => invalid_attributes}, valid_session
+        put :update, {id: @post.to_param, section_id: @post.section.to_param, post: invalid_attributes}, valid_session
         expect(response).to render_template("edit")
       end
     end
   end
 
   describe "DELETE destroy" do
+    login_user
+
     it "destroys the requested post" do
-      post = Post.create! valid_attributes
       expect {
-        delete :destroy, {:id => post.to_param}, valid_session
+        delete :destroy, {:id => @post.to_param, section_id: @post.section.to_param}, valid_session
       }.to change(Post, :count).by(-1)
     end
 
     it "redirects to the posts list" do
-      post = Post.create! valid_attributes
-      delete :destroy, {:id => post.to_param}, valid_session
+      delete :destroy, {id: @post.to_param, section_id: @post.section.to_param}, valid_session
       expect(response).to redirect_to(posts_url)
     end
   end
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,9 +1,9 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
-
+require 'faker'
 FactoryGirl.define do
   factory :post do
-    title "MyString"
-    body "MyText"
+    title Faker::Lorem.words(4)
+    body Faker::Lorem.words(20)
     association :user
     association :section
   end
