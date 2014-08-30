475a3b879f5af305ad7a605cd065c50fdd831c33
Mark Gandolfo <mark@aussiev8.com.au>
2014-08-30 17:47:17 +1000

    set up the devise helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,6 +23,8 @@ RSpec.configure do |config|
   config.infer_base_class_for_anonymous_controllers = false
   config.order = 'random'
   config.use_transactional_fixtures = false
+
+   config.extend ControllerMacros, :type => :controller
 end
 
 Capybara.javascript_driver = :webkit
