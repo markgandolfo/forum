15b82d455b8eabcde08e02a52ecca41d510fb907
Mark Gandolfo <mark@aussiev8.com.au>
2014-08-31 20:11:19 +1000

    add cancan hack and simplify the admin namespace

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_filter :cancan_hack
+
 
   protected
 
@@ -20,11 +22,21 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  private
+
+  def cancan_hack
+    return if request.get?
+    resource = controller_name.singularize.to_sym
+    method = "#{resource}_params"
+    params[resource] &&= send(method) if respond_to?(method, true)
+  end
+
   # Required for cancan abilities
   def namespace
-    controller_name_segments = params[:controller].split('/')
-    controller_name_segments.pop
-    controller_name_segments.join('/').camelize
+    # controller_name_segments = params[:controller].split('/')
+    # controller_name_segments.pop
+    # controller_name_segments.join('/').camelize
+    params[:controller].split('/').first
   end
 
   def current_ability
