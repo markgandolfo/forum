5feec25947199d44e6c4b932d5293434b82e1fc2
Mark Gandolfo <mark@aussiev8.com.au>
2014-09-07 18:51:23 +1000

    fix user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,13 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
+  sequence :email do |n|
+    "person#{n}@example.com"
+  end
+
   factory :user do
     username                Faker::Internet.user_name
-    email                   Faker::Internet.safe_email
+    email
     password                "Password1"
     password_confirmation   "Password1"
     role                    "member"
