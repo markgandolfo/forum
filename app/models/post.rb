cdb937768dad191a1afd81cd691af93b682bca88
Mark Gandolfo <mark@aussiev8.com.au>
2014-09-27 21:35:09 +1000

    add validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ActiveRecord::Base
   has_many :replies
   belongs_to :user
   belongs_to :section
+
+  validates :body, presence: true
+  validates :body, length: { minimum: 4 }
 end
--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,4 +1,7 @@
 class Reply < ActiveRecord::Base
   belongs_to :post, counter_cache: true
   belongs_to :user
+
+  validates :body, presence: true
+  validates :body, length: { minimum: 4 }
 end
